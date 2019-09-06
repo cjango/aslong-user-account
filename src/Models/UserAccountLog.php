e83670b3c51f41a158baf11345ca9cc75a30d384
小陈叔叔 <chenjxlg@163.com>
2019-09-06 14:12:27 +0800

    新增 user 关联

--- src/Models/UserAccountLog.php
+++ src/Models/UserAccountLog.php
@@ -12,11 +12,34 @@ class UserAccountLog extends Model
         'source' => 'array',
     ];
 
+    /**
+     * 关联用户
+     * @Author:<C.Jason>
+     * @Date:2019-09-06T14:11:41+0800
+     * @return [type] [description]
+     */
+    public function user()
+    {
+        return $this->belongsTo(config('user_account.user_model'));
+    }
+
+    /**
+     * 关联账户
+     * @Author:<C.Jason>
+     * @Date:2019-09-06T14:11:52+0800
+     * @return [type] [description]
+     */
     public function account()
     {
         return $this->belongsTo(UserAccount::class, 'user_id', 'user_id');
     }
 
+    /**
+     * 关联规则
+     * @Author:<C.Jason>
+     * @Date:2019-09-06T14:11:59+0800
+     * @return [type] [description]
+     */
     public function rule()
     {
         return $this->belongsTo(UserAccountRule::class)->withDefault();
