b9e0a07ec33795d89cbf7c6db672589a59e0d047
小陈叔叔 <chenjxlg@163.com>
2019-09-18 13:27:01 +0800

    Update UserAccountRule.php

--- src/Models/UserAccountRule.php
+++ src/Models/UserAccountRule.php
@@ -16,4 +16,25 @@ class UserAccountRule extends Model
     {
         return $this->hasMany(UserAccountLog::class, 'rule_id');
     }
+    
+    /**
+     * Notes: 获取触发次数的文字说明
+     * @Author: <C.Jason>
+     * @Date: 2019/9/18 13:24
+     * @return string
+     */
+    protected function getTriggerTextAttribute()
+    {
+        switch ($this->trigger <=> 0) {
+            case -1:
+                return '仅一次';
+                break;
+            case 0:
+                return '不限制';
+                break;
+            case 1:
+                return $this->trigger . ' 次/日';
+                break;
+        }
+    }
 }
